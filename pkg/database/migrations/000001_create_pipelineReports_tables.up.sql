003b08c10a55f2bb1d33c9473d9bff5c74ff5e61
Olivier Vernin <olivier.vernin@suse.com>
2025-06-08 15:56:17 +0200

    Revert "chore: cleanup database migration 01 & 02"
    This reverts commit e26c8716a31cdd6109ed24a0c5d43b7f5df98688.

--- pkg/database/migrations/000001_create_pipelineReports_tables.up.sql
+++ pkg/database/migrations/000001_create_pipelineReports_tables.up.sql
@@ -1,10 +1,11 @@
 BEGIN;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE TABLE IF NOT EXISTS pipelineReports
-(
-   id uuid DEFAULT uuid_generate_v4(),
+CREATE TABLE IF NOT EXISTS pipelineReports(
+   id uuid DEFAULT uuid_generate_v4 (),
    data JSON NOT NULL,
-   created_at TIMESTAMPTZ DEFAULT NOW(),
-   updated_at TIMESTAMPTZ DEFAULT NOW()
+   created_at TIMESTAMP,
+   updated_at TIMESTAMP
 );
+ALTER TABLE pipelineReports ALTER COLUMN created_at SET DEFAULT now();
+ALTER TABLE pipelineReports ALTER COLUMN updated_at SET DEFAULT now();
 COMMIT;
--- pkg/database/migrations/000002_create_scms_tables.up.sql
+++ pkg/database/migrations/000002_create_scms_tables.up.sql
@@ -1,12 +1,12 @@
 BEGIN;
-
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-CREATE TABLE IF NOT EXISTS scms (
-   id uuid DEFAULT uuid_generate_v4(),
+CREATE TABLE IF NOT EXISTS scms(
+   id uuid DEFAULT uuid_generate_v4 (),
    branch VARCHAR NOT NULL,
    url VARCHAR NOT NULL,
-   created_at TIMESTAMPTZ DEFAULT NOW(),
-   updated_at TIMESTAMPTZ DEFAULT NOW()
+   created_at TIMESTAMP,
+   updated_at TIMESTAMP
 );
+ALTER TABLE scms ALTER COLUMN created_at SET DEFAULT now();
+ALTER TABLE scms ALTER COLUMN updated_at SET DEFAULT now();
 COMMIT;
