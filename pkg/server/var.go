8159bcceca733b525742d1982766a74a921f5303
Olivier Vernin <me@olblak.com>
2025-07-18 08:28:53 +0200

    chore: reduce the number of days to monitor reports

--- pkg/server/var.go
+++ pkg/server/var.go
@@ -2,5 +2,8 @@ package server
 
 var (
 	// monitoringDurationDays is the default number of days to search for reports.
-	monitoringDurationDays int = 7
+	// By default we keep this number as low as possible as it directly affects the
+	// performance of the database queries.
+	// The goal is to minimize the impact in small environment
+	monitoringDurationDays int = 2
 )
